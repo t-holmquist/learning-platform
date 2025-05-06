90562d7c371f585a1e5e2ad6f1e3ba0af31bd9f2
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-05-06 19:21:47 +0200

    add shadow styling to theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <main className="flex flex-col gap-10 justify-center items-center min-h-screen">
       <p className="font-satoshi font-black text-5xl">LEARNING PLATFORM</p>
-    </div>
+      <div className="w-40 h-40 hover:shadow-primary hover:translate-x-2 hover:-translate-y-2 transition-all  rounded-xl border"></div>
+    </main>
   );
 }
