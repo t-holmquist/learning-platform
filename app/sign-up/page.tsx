2a400e8f0dd6afa2e85ed1da8dcc073e26abcdaf
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-05-16 15:56:17 +0200

    Fix: apostrophe bug Feat: added animation to sign in card

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,21 +1,35 @@
+'use client'
+
 import Button from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
+import { motion } from 'motion/react';
+
 
 const SignUp = () => {
   return (
     <div className='h-[100vh] sm:overflow-hidden overflow-x-hidden'>
         <nav className='flex justify-between items-center py-2 px-6'>
-            <Image src={"/icons/Logo.svg"} width={250} height={400} alt='BoardCraft Studio Logo' />
+            {/* absolutely posioned to avoid layout shift when logo is loaded that disturbs motion animation */}
+            <Link href={"/"}>
+                <Image className='absolute top-2' src={"/icons/Logo.svg"} width={200} height={30} alt='BoardCraft Studio Logo' /> 
+            </Link>
             <div className='hidden sm:flex gap-2 items-center'>
-                <p className='text-sm'>Don't have an account?</p>
+                <p className='text-sm'>Don&apos;t have an account?</p>
                 <button className='font-medium cursor-pointer hover:text-slate-700'>Sign up here</button>
             </div>
         </nav>
         <section className='flex items-center justify-center h-full'>
-            <div className='flex relative shadow-primary flex-col w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 gap-10 p-6 border rounded-large bg-accent'>
+            <motion.div 
+            initial={{y:20}}
+            animate={{y: 0}}
+            className='flex relative shadow-primary flex-col w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 gap-10 p-6 border rounded-large bg-accent'>
                 {/* Background Dots */}
-                <div className='absolute -z-10 opacity-70 top-10 -left-12 w-[500px] h-[550px] rounded-large bgDots'/>
+                <motion.div 
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                transition={{delay: 0.3}}
+                className='absolute -z-10 opacity-70 top-10 -left-12 w-[500px] h-[550px] rounded-large bgDots'/>
                 <h1 className='font-extrabold text-5xl'>Sign in</h1>
                 <div className='flex flex-col gap-2'>
                     <label className='font-bold' htmlFor="email">Email:*</label>
@@ -43,10 +57,10 @@ const SignUp = () => {
                     </button>
                 </div>
                 <div className='flex gap-2 items-center'>
-                    <p className='text-sm'>Don't have an account?</p>
+                    <p className='text-sm'>Don&apos;t have an account?</p>
                     <button className='font-medium cursor-pointer hover:text-slate-700'>Sign up here</button>
                 </div>
-            </div>
+            </motion.div>
         </section>
     </div>
   )
