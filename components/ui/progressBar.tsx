4ec7992923bf9cd653d40923b72d7505c8b2324a
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-05-20 12:52:52 +0200

    fix ts error

--- components/ui/progressBar.tsx
+++ components/ui/progressBar.tsx
@@ -7,7 +7,7 @@ import {useState} from 'react'
 export const ProgressBar = () => {
   
 // Here it should calculate the amount of completed lessons in LessonData and update the width value
-  const [progress, setProgress] = useState(45);
+  // const [progress, setProgress] = useState(45);
   
   return (
     <div className="flex shadow-primary rounded-large">
@@ -16,7 +16,7 @@ export const ProgressBar = () => {
           width: '0%'
         }}
         animate={{
-          width: `${progress}%`
+          width: `40%`
         }}  
         transition={{
           type: 'spring',
@@ -24,7 +24,7 @@ export const ProgressBar = () => {
           bounce: 0.3,
         }}
         className="bg-secondary rounded-l-xl p-3 border">
-          <p className="text-black font-bold">{progress}%</p>
+          <p className="text-black font-bold">40%</p>
         </motion.div>
         {/* Flex 1 allows the dot div to shrink and grow regardless of initial width */}
         <div className="flex-1 border bgDots border-black rounded-r-xl"></div>
