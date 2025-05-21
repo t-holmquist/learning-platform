455a134669ce97df85b082789c0732aae3712ae3
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-05-21 11:57:32 +0200

    feat: mobile navbar dashboard

--- components/ui/navBarDashboard.tsx
+++ components/ui/navBarDashboard.tsx
@@ -17,22 +17,24 @@ const NavBarDashboard = () => {
 
 
   return (
-    <nav className="hidden sm:flex justify-between items-center px-5 py-2 sm:py-5 sm:px-10 w-full text-center">
+    <nav className="flex justify-between items-center px-5 py-5 sm:px-10 w-full text-center">
         <Link href={"/"}>
             <Image src={"/icons/Logo.svg"} width={200} height={30} alt='BoardCraft Studio Logo' />
         </Link>
         <div className="flex items-center gap-4 font-bold">
-            <button className="cursor-pointer hover:text-slate-700">Course</button>
-            <button className="cursor-pointer hover:text-slate-700">Design Assets</button>
+            <button className="hidden sm:block cursor-pointer hover:text-slate-700">Course</button>
+            <button className="hidden sm:block cursor-pointer hover:text-slate-700">Design Assets</button>
             <button onClick={() => {setIsNotificationOpen(!isNotificationOpen)}} className="relative cursor-pointer">
-                <Image src={"/icons/bell.svg"} width={20} height={20} alt="notifications"/>
+                <Image className="hidden sm:block" src={"/icons/bell.svg"} width={20} height={20} alt="notifications"/>
+                {/* Mobile bell */}
+                <Image className="sm:hidden" src={"/icons/bell.svg"} width={30} height={20} alt="notifications"/>
                 <AnimatePresence>
                     {isNotificationOpen && (
                             <motion.div 
                             initial={{opacity: 0, y: 20}}
                             animate={{opacity: 1, y: 0}}
                             exit={{opacity: 0, y: 20}}
-                            className="absolute flex flex-col p-2 gap-2 -left-65 top-9 border bg-bg-sand rounded-small">
+                            className="absolute flex flex-col p-2 gap-2 -left-65 top-9 border bg-bg-sand z-20 rounded-small">
                             {Notifications.map((message, index) => (
                                 <div key={index} className="flex flex-col gap-2 text-start hover:bg-bg-green p-1 rounded-small">
                                     <div className="flex gap-2 items-center">
@@ -47,7 +49,7 @@ const NavBarDashboard = () => {
                     )}
                 </AnimatePresence>
             </button>
-            <button onClick={() => {setIsSettingsOpen(!isSettingsOpen)}} className="relative border rounded-full cursor-pointer">
+            <button onClick={() => {setIsSettingsOpen(!isSettingsOpen)}} className="hidden sm:block relative border rounded-full cursor-pointer">
                 <Image src={"/mathias.webp"} width={45} height={45} alt="notifications"/>
                 <AnimatePresence>
                     {isSettingsOpen && (
