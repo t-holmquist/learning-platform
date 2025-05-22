9aa51ebccb70eaecd7913d9d33f44f7770a1985b
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-05-22 09:53:11 +0200

    feat: top nav in learn section

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,7 @@
 import { lessonData } from '@/data/data';
 import Link from 'next/link';
 import { Dispatch, SetStateAction } from 'react';
+import Image from 'next/image';
 
 const Sidebar = ( {activeLesson, setActiveLesson} : {
   activeLesson: number;
@@ -11,28 +12,27 @@ const Sidebar = ( {activeLesson, setActiveLesson} : {
 
 
   return (
-    <div className="hidden lg:block sticky top-0 w-[250px] border h-[100vh]">
-      <div className='border-b flex items-center justify-center h-[50px]'>
-        <Link href='/' className='font-extrabold text-xl'>BoardCraft Studio</Link>
+    <div className="hidden lg:block sticky top-0 w-[300px] border h-[100vh]">
+      <Link className='flex items-center justify-center h-[70px]' href={"/"}>
+            <Image src={"/icons/Logo.svg"} width={200} height={30} alt='BoardCraft Studio Logo' />
+      </Link>
+      <div className='bg-accent p-2 text-center font-medium border border-black'>Module: User Testing</div>
+      <div className="flex flex-col">
+        {lessonData.map(({id, title}, idx) => (
+          <button 
+          key={idx}
+          onClick={() => {setActiveLesson(id)}}
+          className={id == activeLesson ? `cursor-pointer text-white bg-bg-green` : `cursor-pointer hover:bg-slate-100 text-white`}
+          >
+            <div className='flex items-center p-2 gap-4'>
+                <div className='p-0.5 border border-black bg-accent rounded-full'>
+                  <svg width="15" height="15" viewBox="0 0 15 15" fill="black" xmlns="http://www.w3.org/2000/svg"><path d="M3.24182 2.32181C3.3919 2.23132 3.5784 2.22601 3.73338 2.30781L12.7334 7.05781C12.8974 7.14436 13 7.31457 13 7.5C13 7.68543 12.8974 7.85564 12.7334 7.94219L3.73338 12.6922C3.5784 12.774 3.3919 12.7687 3.24182 12.6782C3.09175 12.5877 3 12.4252 3 12.25V2.75C3 2.57476 3.09175 2.4123 3.24182 2.32181ZM4 3.57925V11.4207L11.4288 7.5L4 3.57925Z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                </div>
+                <p className='text-black text-sm max-w-[150px] text-start'>{title}</p>
+            </div>
+          </button>
+        ))}
       </div>
-        <div className='bg-accent p-2 text-center font-medium border-b border-black'>Module: User Testing</div>
-        <div className="flex flex-col">
-          {lessonData.map(({id, title}, idx) => (
-            <button 
-            key={idx}
-            onClick={() => {setActiveLesson(id)}}
-            className={id == activeLesson ? `cursor-pointer text-white bg-bg-green` : `cursor-pointer hover:bg-slate-100 text-white`}
-            >
-              <div className='flex items-center p-2 gap-4'>
-                  <div className='p-0.5 border border-black bg-accent rounded-full'>
-                    <svg width="15" height="15" viewBox="0 0 15 15" fill="black" xmlns="http://www.w3.org/2000/svg"><path d="M3.24182 2.32181C3.3919 2.23132 3.5784 2.22601 3.73338 2.30781L12.7334 7.05781C12.8974 7.14436 13 7.31457 13 7.5C13 7.68543 12.8974 7.85564 12.7334 7.94219L3.73338 12.6922C3.5784 12.774 3.3919 12.7687 3.24182 12.6782C3.09175 12.5877 3 12.4252 3 12.25V2.75C3 2.57476 3.09175 2.4123 3.24182 2.32181ZM4 3.57925V11.4207L11.4288 7.5L4 3.57925Z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                  </div>
-                  <p className='text-black text-sm max-w-[150px] text-start'>{title}</p>
-              </div>
-            </button>
-          ))}
-          <p className='py-20 font-extrabold'>Current Lesson: {activeLesson}</p>
-        </div>
     </div>
   )
 }
---
+++ components/ui/navBarCourse.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import Image from "next/image";
+import { useState } from "react";
+import { AnimatePresence, motion } from 'motion/react';
+
+
+
+const NavBarCourse = () => {
+
+    const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+    const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+
+    const Notifications = ['Check out our latest course', 'This article might inspire you', 'Get started with user testing']
+    const Settings = ['Account', 'Settings', 'Theme']
+
+
+  return (
+    <nav className="flex justify-between items-center h-[72px] px-5 w-full text-center border-t border-b">
+        <p>Lektionsinfo</p>
+        <div className="flex items-center gap-4 font-bold">
+            <button className="hidden sm:block cursor-pointer hover:text-slate-600">Course</button>
+            <button className="hidden sm:block cursor-pointer hover:text-slate-600">Design Assets</button>
+            <button onClick={() => {setIsNotificationOpen(!isNotificationOpen); setIsSettingsOpen(false)}} className="relative cursor-pointer">
+                <Image className="hidden sm:block" src={"/icons/bell.svg"} width={20} height={20} alt="notifications"/>
+                {/* Mobile bell */}
+                <Image className="sm:hidden" src={"/icons/bell.svg"} width={30} height={20} alt="notifications"/>
+                <AnimatePresence>
+                    {isNotificationOpen && (
+                            <motion.div 
+                            initial={{opacity: 0, y: 20}}
+                            animate={{opacity: 1, y: 0}}
+                            exit={{opacity: 0, y: 20}}
+                            className="absolute flex flex-col p-2 gap-2 -left-65 top-9 border bg-bg-sand z-20 rounded-small">
+                            {Notifications.map((message, index) => (
+                                <div key={index} className="flex flex-col gap-2 text-start hover:bg-bg-green p-1 rounded-small">
+                                    <div className="flex gap-2 items-center">
+                                        <div className={`${index == 0 && 'bg-red-500'} border w-1.5 h-1.5 rounded-full`}></div>
+                                        <p className="text-sm font-medium ">{message}</p>
+                                    </div>
+                                    <p className="text-xs font-light">This is a new Notifications about something interesting that is useful in a way</p>
+                                </div>
+                                ))}
+                            </motion.div>
+                        
+                    )}
+                </AnimatePresence>
+            </button>
+            <button onClick={() => {setIsSettingsOpen(!isSettingsOpen); setIsNotificationOpen(false)}} className="hidden sm:block relative border rounded-full cursor-pointer">
+                <Image src={"/mathias.webp"} width={45} height={45} alt="notifications"/>
+                <AnimatePresence>
+                    {isSettingsOpen && (
+                            <motion.div 
+                            initial={{opacity: 0, y: 20}}
+                            animate={{opacity: 1, y: 0}}
+                            exit={{opacity: 0, y: 20}}
+                            className="absolute flex flex-col p-2 gap-2 -left-13 top-12 border bg-bg-sand rounded-small">
+                            {Settings.map((message, index) => (
+                                <div key={index} className="flex flex-col gap-2 text-start hover:bg-bg-green p-3 rounded-small">
+                                    <div className="flex gap-2 items-center">
+                                        <p className="text-sm font-medium ">{message}</p>
+                                    </div>
+                                </div>
+                                ))}
+                            </motion.div>
+                    )}
+                </AnimatePresence>
+            </button>
+        </div> 
+    </nav>
+  )
+}
+
+export default NavBarCourse;
