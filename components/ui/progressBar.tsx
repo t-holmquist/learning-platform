faad01fd5dcabcfc2ca7ffccef130589c1629f3b
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-05-22 16:36:55 +0200

    feat: enhance ProgressBar to calculate default completion percentage when not provided

--- components/ui/progressBar.tsx
+++ components/ui/progressBar.tsx
@@ -1,13 +1,25 @@
 'use client'
 
+import { lessonData } from '@/data/data';
 import { motion } from 'motion/react'
 
 
 export const ProgressBar = ( { completedLessons } : {
-  completedLessons: string;
+  completedLessons?: string;
 } ) => {
   
-  // const [progress, setProgress] = useState(45);
+  // Calculate amount of completed lessons. Currently not connected to database, just showing the static data from lessonData.
+  // If completed prop given then show that percentage or else calculate based on static data.
+  let lessonsCompleted = 0;
+  const totalLessons = lessonData.length;
+  
+  for (let i = 0; i < lessonData.length; i++) {
+    if (lessonData[i].completed) {
+      lessonsCompleted += 1
+    } 
+  }
+
+  const defaultCompletedLessons = Math.floor((lessonsCompleted / totalLessons) * 100 ).toString()
   
   return (
     <div className="flex shadow-primary rounded-large">
@@ -16,7 +28,7 @@ export const ProgressBar = ( { completedLessons } : {
           width: '0%'
         }}
         animate={{
-          width: `${completedLessons}%`
+          width: `${completedLessons ? `${completedLessons}` : `${defaultCompletedLessons}`}%`
         }}  
         transition={{
           type: 'spring',
@@ -24,7 +36,7 @@ export const ProgressBar = ( { completedLessons } : {
           bounce: 0.3,
         }}
         className="bg-secondary rounded-l-xl p-3 border">
-          <p className="text-black font-bold">{completedLessons}%</p>
+          <p className="text-black font-bold">{completedLessons ? `${completedLessons}` : `${defaultCompletedLessons}`}%</p>
         </motion.div>
         {/* Flex 1 allows the dot div to shrink and grow regardless of initial width */}
         <div className="flex-1 border bgDots border-black rounded-r-xl"></div>
