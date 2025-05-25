939d72a79bbefd91ac94f3a10582b84a12b32a43
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-05-25 10:32:45 +0200

    fix: adjust video component width for consistent styling

--- components/ui/showcaseVideo.tsx
+++ components/ui/showcaseVideo.tsx
@@ -19,7 +19,7 @@ export const ShowcaseVideo = () => {
     // Using css group class to make hover-effects apply to children based on parent hover state. Removes the need for js.
     className='group col-span-4 row-span-6 border space-y-4 rounded-large p-6 bg-bg-green hover:border-primary transition-all'>
         <h2 className='text-2xl font-bold'>Videos packed with expert knowledge</h2>
-        <video id="video" onMouseEnter={() => {handlePlay()}} className="border bg-bg-green group-hover:-translate-y-1 group-hover:shadow-primary transition-all rounded-large aspect-video" muted width="400" height="200" preload="auto">
+        <video id="video" onMouseEnter={() => {handlePlay()}} className="border bg-bg-green w-full group-hover:-translate-y-1 group-hover:shadow-primary transition-all rounded-large aspect-video" muted width="400" height="200" preload="auto">
             <source src="/introductionLesson-video.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
