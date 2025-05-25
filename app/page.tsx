89f72696e554e708277b8385e963babf767804a6
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-05-25 10:28:34 +0200

    feat: enhance Home page layout and implement showcaseVideo component

--- app/page.tsx
+++ app/page.tsx
@@ -14,23 +14,21 @@ import { ShowcaseVideo } from '@/components/ui/showcaseVideo';
 const Home = () => {
   return (
     // Dot background overflows on mobile -> overflow-x-hidden. No scroll on tablet and desktop -> h-100vh
-    <div className='font-satoshi sm:min-h-screen flex flex-col gap-6 md:gap-10 justify-between overflow-x-hidden'>
+    <div className='font-satoshi sm:min-h-screen flex flex-col gap-8 md:gap-10 justify-between overflow-x-hidden'>
+
         <nav className='flex justify-between items-center px-5 py-5 sm:px-10'>
             <Link href={"/"}>
                 <Image src={"/icons/Logo.svg"} width={200} height={30} alt='BoardCraft Studio Logo' /> 
             </Link>
             <div className='flex gap-2 sm:gap-6 items-center'>
-                <button className='font-bold cursor-pointer hover:bg-bg-sandHover p-2 border rounded-small'>Sign up</button>
+                <button className='font-bold cursor-pointer hover:bg-bg-sandHover py-2 px-4 border rounded-small'>Sign up</button>
                 <button className='font-medium cursor-pointer hover:text-slate-700'>Log in</button>
             </div>
         </nav>
         <div className='w-full flex flex-col items-center'>
-          <section className='space-y-6 w-11/12 lg:w-7/12 flex flex-col items-center'>
-            <h1 className='font-black sm:leading-20 text-center text-3xl sm:text-6xl'>Design, Build & Market Your Dream Board Game</h1>
+          <section className='space-y-10 w-11/12 lg:w-7/12 flex flex-col items-center'>
+            <h1 className='font-black sm:leading-20 text-center text-5xl sm:text-6xl'>Design, Build & Market Your Dream Board Game</h1>
             <p className='sm:text-center sm:text-lg'>Master the art of board game creation with interactive courses, expert guidance, and a community of passionate designers. From concept to published game.</p>
-            <Link href={"/sign-in"}>
-              <Button className='w-fit' title='Get started'/>
-            </Link>
           </section>
         </div>
         <section className='grid grid-cols-1 lg:grid-cols-12 gap-10 grid-rows-12 px-5 py-5 sm:px-10'>
@@ -45,7 +43,15 @@ const Home = () => {
               {/* Video learning */}
               <ShowcaseVideo />
         </section>
-        <Footer/>
+        <div className='w-full flex flex-col items-center'>
+          <section className='space-y-12 mb-20 w-11/12 lg:w-7/12 flex flex-col items-center'>
+            <h2 className='font-black text-center text-2xl sm:text-4xl'>Ready to see your game idea become a reality?</h2>
+            <Link href={"/sign-in"}>
+              <Button className='w-fit' title='Try our platform now'/>
+            </Link>
+          </section>
+        </div>
+        <Footer />
     </div>
   )
 }
--- components/ui/showcaseVideo.tsx
+++ components/ui/showcaseVideo.tsx
@@ -1,27 +1,28 @@
 
 export const ShowcaseVideo = () => {
 
-  return (
 
+    const handlePlay = async () => {
+       try {
+        const video = document.getElementById('video') as HTMLVideoElement | null;
+        if (video) {
+            video.play();
+        } 
+
+       } catch (error) {
+        console.log(error)
+       } 
+    }
+
+  return (
     <div 
     // Using css group class to make hover-effects apply to children based on parent hover state. Removes the need for js.
-    className='group col-span-4 row-span-6 border space-y-4 rounded-large p-6 bg-secondary hover:border-primary transition-all'>
-        <h2 className='text-2xl font-bold'>Get XP and rank up</h2>
-        <div
-        className='flex flex-col px-4 p-8 gap-4 group-hover:rotate-z-3  transition-all  border rounded-large bg-bg-red shadow-primary'>
-            <div className="flex justify-between items-center">
-                <h3 className='text-xl font-bold'>Current XP:</h3>
-                <p className="text-sm text-primary">1287 / 2700 XP</p>
-            </div>
-            <div className="flex justify-between items-center">
-                <h3 className='text-xl font-bold'>Rank</h3>
-                <p className="text-sm text-primary">Dreamer</p>
-            </div>
-            <div className="flex justify-between items-center">
-                <h3 className='text-xl font-bold'>Next rank</h3>
-                <p className="text-sm text-primary">Architect</p>
-            </div>
-        </div>
+    className='group col-span-4 row-span-6 border space-y-4 rounded-large p-6 bg-bg-green hover:border-primary transition-all'>
+        <h2 className='text-2xl font-bold'>Videos packed with expert knowledge</h2>
+        <video id="video" onMouseEnter={() => {handlePlay()}} className="border bg-bg-green group-hover:-translate-y-1 group-hover:shadow-primary transition-all rounded-large aspect-video" muted width="400" height="200" preload="auto">
+            <source src="/introductionLesson-video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
     </div>
     
   );
