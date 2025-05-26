360f3995f2b24be1163204338b71c2c894d22380
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-05-26 13:22:29 +0200

    reduced thumbnail size video

--- components/ui/video.tsx
+++ components/ui/video.tsx
@@ -1,6 +1,6 @@
 export function Video() {
   return (
-    <video className="border bg-bg-green rounded-large aspect-video" width="1280" height="720" poster="/thumbnail-Introduction.jpg" controls preload="none">
+    <video className="border bg-bg-green rounded-large aspect-video" width="1280" height="720" poster="/thumbnail.webp" controls preload="none">
       <source src="/introduction-video.webm" type="video/webm" />
       <source src="/introductionLesson-video.mp4" type="video/mp4" />
       Your browser does not support the video tag.
