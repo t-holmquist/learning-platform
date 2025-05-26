3bf35a74bc1f1668b19721e1c00f5f63186178f6
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-05-26 13:03:05 +0200

    added webm video for improved performance

--- components/ui/video.tsx
+++ components/ui/video.tsx
@@ -1,6 +1,7 @@
 export function Video() {
   return (
     <video className="border bg-bg-green rounded-large aspect-video" width="1280" height="720" poster="/thumbnail-Introduction.jpg" controls preload="none">
+      <source src="/introduction-video.webm" type="video/webm" />
       <source src="/introductionLesson-video.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
